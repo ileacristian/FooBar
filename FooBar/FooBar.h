a407f7dc73db8db811d1a0fe4f37c31c4a91610c
Cristian Ilea <cristianilea@192-168-0-112.rdsnet.ro>
2016-08-08 18:55:30 +0300

    initial commit

---
+++ FooBar/FooBar.h
@@ -0,0 +1,19 @@
+//
+//  FooBar.h
+//  FooBar
+//
+//  Created by Cristian Ilea on 8/8/16.
+//  Copyright © 2016 Cristian Ilea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FooBar.
+FOUNDATION_EXPORT double FooBarVersionNumber;
+
+//! Project version string for FooBar.
+FOUNDATION_EXPORT const unsigned char FooBarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FooBar/PublicHeader.h>
+
+
---
+++ FooBar/FooBar.swift
@@ -0,0 +1,21 @@
+//
+//  FooBar.swift
+//  FooBar
+//
+//  Created by Cristian Ilea on 8/8/16.
+//  Copyright © 2016 Cristian Ilea. All rights reserved.
+//
+
+import Foundation
+import Curry
+
+
+func threeWords(a:String, b:String, c:String) -> String
+{
+    return a + " " + b + " " + c
+}
+
+public func fooBar() -> String
+{
+    return curry(threeWords)("foo")("bar")("baz")
+}
